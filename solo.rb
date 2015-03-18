c74c1041ca3dadd6bd933da4759d90977422228d
Jorge Quilcate <quilcate.jorge@gmail.com>
2015-03-18 23:23:41 +0100

    chef solo files

---
+++ solo.rb
@@ -0,0 +1,18 @@
+cookbook_path [
+               "/home/jeqo/dev/chef-repo/cookbooks"
+              ]
+data_bag_path "/home/jeqo/dev/chef-repo/data_bags"
+environment_path "/home/jeqo/dev/chef-repo/environments"
+file_backup_path "/home/jeqo/dev/chef-repo/backup"
+file_cache_path "/home/jeqo/dev/chef-repo/cache"
+json_attribs nil
+lockfile nil
+log_level :info
+log_location STDOUT
+node_name "jeqo-linux"
+rest_timeout 300
+role_path "/home/jeqo/dev/chef-repo/roles"
+solo true
+syntax_check_cache_path
+umask 0022
+verbose_logging nil
