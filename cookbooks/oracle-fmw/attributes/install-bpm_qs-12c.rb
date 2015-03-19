8fbb055900780e5bb4d5ad2dcfd0b9dba7868f5b
Jorge Quilcate <quilcate.jorge@gmail.com>
2015-03-19 01:06:17 +0100

    new checksum sha256 bpm

--- cookbooks/oracle-fmw/attributes/install-bpm_qs-12c.rb
+++ cookbooks/oracle-fmw/attributes/install-bpm_qs-12c.rb
@@ -1,3 +1,3 @@
 default['bpm_qs-12c']['version'] = "12.1.3"
 default['bpm_qs-12c']['url'] = "file:///data/oracle-fmw/bpm_qs-12c/fmw_#{node['bpm_qs-12c']['version']}.0.0_bpmqs_Disk1_1of1.zip"
-default['bpm_qs-12c']['checksum'] = "32fafb1eb4f696f3d8d1da932c969a44ae0eacf2"
+default['bpm_qs-12c']['checksum'] = "DC0270122D52EB9F0C8A1349F1243794F4692105DBBD9AFFED9D845A4452ADE5"
