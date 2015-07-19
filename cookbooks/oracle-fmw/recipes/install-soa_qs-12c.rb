52369750f76cf24f12bbd632e9e101e48a3cd6f8
Jorge Quilcate <quilcate.jorge@gmail.com>
2015-07-19 22:28:22 +0200

    Add support to Oracle SOA QS 12c

---
+++ cookbooks/oracle-fmw/recipes/install-soa_qs-12c.rb
@@ -0,0 +1,43 @@
+include_recipe "oracle-fmw::prepare-environment-12c"
+
+os_user = node['fmw-12c']['os_user']
+os_installer_group = node['fmw-12c']['os_installer_group']
+
+# Unzip installers
+installer_zip = File.join(Chef::Config[:file_cache_path], "fmw_#{node['soa_qs-12c']['version']}.0.0_bpmqs_Disk1_1of1.zip")
+installer_jar_path = File.join(Chef::Config[:file_cache_path], "fmw_#{node['soa_qs-12c']['version']}.0.0_bpm_quickstart.jar")
+
+unzip_command = "unzip -o #{installer_zip}"
+
+remote_file installer_zip do
+	source node['soa_qs-12c']['url']
+	checksum node['soa_qs-12c']['checksum']
+	action :create
+end
+
+execute unzip_command do
+	cwd Chef::Config[:file_cache_path]
+	action :run
+	creates installer_jar_path
+end
+
+# Create Response File
+response_file = File.join(Chef::Config[:file_cache_path], "soa_qs-12c.rsp")
+
+template response_file do
+	source "soa_qs-12c.rsp.erb"
+	variables({
+		:oracle_home => node['fmw-12c']['home']
+	})
+end
+
+# Install BPM QuickStart
+install_command = "java -jar #{installer_jar_path} -silent -responseFile #{response_file} -invPtrLoc #{node['fmw-12c']['orainventory_file']}"
+
+execute install_command do
+	cwd Chef::Config[:file_cache_path]
+	user os_user
+	group os_installer_group
+	action :run
+	creates "#{node['fmw-12c']['home']}/oraInst.loc"
+end
