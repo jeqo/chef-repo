d22d38733aa6f2994e8ca3e8399661e76c95d2c2
Jorge Quilcate <quilcate.jorge@gmail.com>
2015-07-19 23:34:06 +0200

    Fix SOA QS 12c variables

--- cookbooks/oracle-fmw/attributes/install-soa_qs-12c.rb
+++ cookbooks/oracle-fmw/attributes/install-soa_qs-12c.rb
@@ -1,3 +1,3 @@
 default['soa_qs-12c']['version'] = "12.1.3"
-default['soa_qs-12c']['url'] = "file:///data/oracle-fmw/soa_qs-12c/fmw_#{node['soa_qs-12c']['version']}.0.0_bpmqs_Disk1_1of1.zip"
-default['soa_qs-12c']['checksum'] = "DC0270122D52EB9F0C8A1349F1243794F4692105DBBD9AFFED9D845A4452ADE5"
+default['soa_qs-12c']['url'] = "file:///data/oracle-fmw/soa_qs-12c/fmw_#{node['soa_qs-12c']['version']}.0.0_soaqs_Disk1_1of1.zip"
+default['soa_qs-12c']['checksum'] = "3867ae18b12c"
--- cookbooks/oracle-fmw/recipes/install-soa_qs-12c.rb
+++ cookbooks/oracle-fmw/recipes/install-soa_qs-12c.rb
@@ -4,8 +4,8 @@ os_user = node['fmw-12c']['os_user']
 os_installer_group = node['fmw-12c']['os_installer_group']
 
 # Unzip installers
-installer_zip = File.join(Chef::Config[:file_cache_path], "fmw_#{node['soa_qs-12c']['version']}.0.0_bpmqs_Disk1_1of1.zip")
-installer_jar_path = File.join(Chef::Config[:file_cache_path], "fmw_#{node['soa_qs-12c']['version']}.0.0_bpm_quickstart.jar")
+installer_zip = File.join(Chef::Config[:file_cache_path], "fmw_#{node['soa_qs-12c']['version']}.0.0_soaqs_Disk1_1of1.zip")
+installer_jar_path = File.join(Chef::Config[:file_cache_path], "fmw_#{node['soa_qs-12c']['version']}.0.0_soa_quickstart.jar")
 
 unzip_command = "unzip -o #{installer_zip}"
 
@@ -31,7 +31,7 @@ template response_file do
 	})
 end
 
-# Install BPM QuickStart
+# Install soa QuickStart
 install_command = "java -jar #{installer_jar_path} -silent -responseFile #{response_file} -invPtrLoc #{node['fmw-12c']['orainventory_file']}"
 
 execute install_command do
